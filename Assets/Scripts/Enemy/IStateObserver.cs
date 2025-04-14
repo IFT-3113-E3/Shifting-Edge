98a00fa49683fce012983fe64234fa5485384e57
MasterLaplace <guillaume.papineau@epitech.eu>
2025-04-14 18:53:41 -0400

    delete: lfs references in Assets folder

--- Assets/Scripts/Enemy/IStateObserver.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Enemy
-{
-    public interface IStateObserver<TContext>
-    {
-        void OnStateEnter(State<TContext> state);
-        void OnStateExit(State<TContext> state);
-        void OnStateUpdate(State<TContext> state);
-    }
-}
