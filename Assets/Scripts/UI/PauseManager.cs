4ed66d70c9dedbeb709b4731f7bf46099af2ed63
andoxin <anghjulinacasa@gmail.com>
2025-04-15 12:55:01 -0400

    Update pause manager

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -3,19 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuManager : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    [Header("UI Elements")]
-    public GameObject pauseUI;               // Ton Canvas de menu pause
-    public RawImage pauseBlurImage;          // RawImage floue à activer pendant la pause
-
-    [Header("Camera Control")]
-    public OrbitCamera orbitCamera;
-    // public CameraPauseOffset cameraPauseOffset;  // Ton script d'offset sur la caméra
-=======
     public GameObject pauseMenuPanel;
     public GameObject optionsPanel;
     public GameObject characterPanel;
->>>>>>> Stashed changes
 
     private bool isPaused = false;
 
@@ -34,19 +24,7 @@ public class PauseMenuManager : MonoBehaviour
     {
         pauseMenuPanel.SetActive(true);
         Time.timeScale = 0f;
-<<<<<<< Updated upstream
-
-        pauseUI.SetActive(true);
-        pauseBlurImage.enabled = true;
-
-        if (orbitCamera != null)
-            orbitCamera.enabled = false;
-
-        // if (cameraPauseOffset != null)
-        //     cameraPauseOffset.EnterPause();
-=======
         isPaused = true;
->>>>>>> Stashed changes
     }
 
     public void ResumeGame()
@@ -55,28 +33,11 @@ public class PauseMenuManager : MonoBehaviour
         optionsPanel.SetActive(false);
         characterPanel.SetActive(false);
         Time.timeScale = 1f;
-<<<<<<< Updated upstream
-
-        pauseUI.SetActive(false);
-        pauseBlurImage.enabled = false;
-
-        if (orbitCamera != null)
-            orbitCamera.enabled = true;
-
-        // if (cameraPauseOffset != null)
-        //     cameraPauseOffset.ExitPause();
-=======
         isPaused = false;
->>>>>>> Stashed changes
     }
 
     public void OpenOptions()
     {
-<<<<<<< Updated upstream
-        // if (!isPaused || cameraPauseOffset == null) return;
-
-        // cameraPauseOffset.Hover(direction);
-=======
         optionsPanel.SetActive(true);
         pauseMenuPanel.SetActive(false);
     }
@@ -96,6 +57,5 @@ public class PauseMenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
->>>>>>> Stashed changes
     }
 }
