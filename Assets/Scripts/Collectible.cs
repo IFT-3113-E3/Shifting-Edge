0ec645872196ffc8cbdcfcb9e23b99953b185e40
Master_Laplace <60772783+MasterLaplace@users.noreply.github.com>
2025-04-30 05:15:41 -0400

    feat: emission and lighting management added for collectable objects (#41)

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -5,6 +5,31 @@ public class Collectible : MonoBehaviour
     public static int totalCollected = 0;
 
     private bool isCollected = false;
+    [SerializeField] private GameObject cristal;
+    private Material cristalMaterial;
+    private Light pointLight;
+
+    void Start()
+    {
+        if (cristal != null)
+        {
+            if (cristal.TryGetComponent<Renderer>(out var renderer))
+            {
+                cristalMaterial = renderer.material;
+                DisableEmission();
+            }
+
+            var pointLightTransform = cristal.transform.Find("Point Light");
+            if (pointLightTransform != null)
+            {
+                pointLight = pointLightTransform.GetComponent<Light>();
+                if (pointLight != null)
+                {
+                    pointLight.enabled = false;
+                }
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,7 +39,33 @@ public class Collectible : MonoBehaviour
         {
             isCollected = true;
             totalCollected++;
+            EnableEmission();
+            EnablePointLight();
             Destroy(gameObject);
+        }
+    }
+
+    private void DisableEmission()
+    {
+        if (cristalMaterial != null)
+        {
+            cristalMaterial.DisableKeyword("_EMISSION");
+        }
+    }
+
+    private void EnableEmission()
+    {
+        if (cristalMaterial != null)
+        {
+            cristalMaterial.EnableKeyword("_EMISSION");
+        }
+    }
+
+    private void EnablePointLight()
+    {
+        if (pointLight != null)
+        {
+            pointLight.enabled = true;
         }
     }
 }
