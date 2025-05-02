c9d6806d983f7735148a39085efe45b75f4d69a8
Jonathan Richard <jwrichard26@gmail.com>
2025-05-02 14:03:08 -0400

    feat: Add regeneration status effect and passive regeneration applier

---
+++ Assets/Scripts/Status/PassiveRegenerationApplier.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace Status
+{
+    public class PassiveRegenerationApplier : MonoBehaviour
+    {
+        public float healthPerSecond = 5f;
+        public float waitTime = 5f;
+        
+        private EntityStatus _entityStatus;
+        private float _timeSinceLastHit = 0f;
+        private bool _isRegenerating = false;
+
+        private void Awake()
+        {
+            _entityStatus = GetComponent<EntityStatus>();
+        }
+        
+        private void OnEnable()
+        {
+            _entityStatus.OnDamageTaken += OnHit;
+        }
+        
+        private void OnDisable()
+        {
+            _entityStatus.OnDamageTaken -= OnHit;
+        }
+        
+        private void Update()
+        {
+            if (_entityStatus.IsDead) return;
+            if (_entityStatus.CurrentHealth >= _entityStatus.maxHealth)
+            {
+                _timeSinceLastHit = 0f;
+                
+                ClearEffect();
+            }
+
+            if (_isRegenerating) return;
+            _timeSinceLastHit += Time.deltaTime;
+
+            if (_timeSinceLastHit >= waitTime)
+            {
+                Debug.Log("Regenerating health");
+                _timeSinceLastHit = 0f;
+                _isRegenerating = true;
+                ApplyPassiveRegeneration();
+            }
+        }
+        
+        private void ApplyPassiveRegeneration()
+        {
+            if (_entityStatus.CurrentHealth < _entityStatus.maxHealth)
+            {
+                _entityStatus.AddEffect(new RegenerationStatusEffect(healthPerSecond, float.MaxValue, true));
+            }
+        }
+        
+        private void ClearEffect()
+        {
+            _isRegenerating = false;
+            _entityStatus.RemoveEffect("Regeneration");
+        }
+        
+        private void OnHit(DamageRequest damageRequest)
+        {
+            _timeSinceLastHit = 0f;
+            if (_isRegenerating)
+            {
+                ClearEffect();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Status/RegenerationStatusEffect.cs
@@ -0,0 +1,53 @@
+﻿namespace Status
+{
+    public class RegenerationStatusEffect : IStatusEffect
+    {
+        private EntityStatus _entityStatus;
+        private float _duration;
+        private float _healthPerSecond;
+        private float _elapsedTime;
+        private bool _isContinuous;
+        
+        public RegenerationStatusEffect(float healthPerSecond, float duration, bool continuous = false)
+        {
+            _duration = duration;
+            _healthPerSecond = healthPerSecond;
+            _isContinuous = continuous;
+        }
+        
+        public string Id => "Regeneration";
+        public bool IsFinished => _duration <= 0;
+
+        public void OnApply(EntityStatus manager)
+        {
+            _entityStatus = manager;
+        }
+
+        public void Tick(float deltaTime)
+        {
+            if (_entityStatus == null) return;
+
+            _duration -= deltaTime;
+
+            if (!_isContinuous)
+            {
+                _elapsedTime += deltaTime;
+            
+                if (_elapsedTime > 1f)
+                {
+                    _elapsedTime = 0;
+                    _entityStatus.Heal(_healthPerSecond);
+                }                
+            }
+            else
+            {
+                _entityStatus.Heal(_healthPerSecond * deltaTime);
+            }
+        }
+
+        public void OnRemove(EntityStatus manager)
+        {
+            _entityStatus = null;
+        }
+    }
+}
